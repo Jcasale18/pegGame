02e8153721b78146de28fae9beedbda4efb2300d
Joseph Casale <jcasale5@student.monroecc.edu>
2021-03-26 13:13:03 -0400

    adding some javaDoc

--- peggame/PegGame.java
+++ peggame/PegGame.java
@@ -3,7 +3,20 @@ package peggame;
 import java.util.Collection;
 
 public interface PegGame {
+    /**
+     * Find all moves on a board.
+     * @return  a collection of all possible moves on a board.
+     */
     Collection<Move> getPossibleMoves();
+    /**
+     * Get the state of the game; may have to call analyzeGameState.
+     * @return returns gamestate
+     */
     GameState getGameState();
+    /**
+     * Make a move
+     * @param move Move object 
+     * @throws PegGameException
+     */
     void makeMove(Move move) throws PegGameException;
 }
