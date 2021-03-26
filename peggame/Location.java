457087d30aca9b189bb14561fe67ab870be8081e
Quinn <69921208+qmt8843@users.noreply.github.com>
2021-03-26 13:01:27 -0400

    Adding javadoc to location

--- peggame/Location.java
+++ peggame/Location.java
@@ -7,13 +7,27 @@ public class Location {
         this.row = row;
         this.col = col;
     }
+
+    /**
+     * Returns the location's row
+     * @return The location's row
+     */
     public int getRow() {
         return row;
     }
 
+    /**
+     * Returns the location's column
+     * @return The location's column
+     */
     public int getCol() {
         return col;
     }
+
+    /**
+     * Checks to see if another location is the same by
+     * comparing their row & column
+     */
     @Override
     public boolean equals(Object o){
         if(o instanceof Location){
@@ -22,12 +36,23 @@ public class Location {
         }
         return false;
     }
+
+    /**
+     * In order for each location to be uniquly identifiable, up to 10001 pieces,
+     * mulitplies the row by 10k and adds the column
+     */
     @Override
     public int hashCode(){
         return row*10000 + col;//10001 column board allowed
     }
     //Methods for retrieving neighbors
 
+
+    /**
+     * Gets the theoretical neighbor of a location by given direction
+     * @param dir A keyword to get the direction of neighbor - L, R, T, B , TR, TL, BR, BL
+     * @return A new location with the coordinates it should have, given the direction
+     */
     public Location getNeighbor(String dir){
         dir = dir.toUpperCase();
         if (dir.equals("L")){
@@ -50,9 +75,12 @@ public class Location {
         return null;
     }
 
+
+    /**
+     * Returns a string of the coordinates of the location
+     */
     @Override
     public String toString(){
         return "("+this.row+", "+this.col+")";
-    }
-    
+    }   
 }
